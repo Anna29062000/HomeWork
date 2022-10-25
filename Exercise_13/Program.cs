97b955d0ff4638afef1cd74b429fde9067c8d3d4
Ann <annakuznetsova419@gmail.com>
2022-10-25 16:21:47 +1000

    HW2

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число...");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    
+    Console.WriteLine($"Третья цифра числа: {number}");
+}
---
+++ Exercise_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели...");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number < 1) | (number > 7))
+{
+    Console.WriteLine("Вы ввели цифру, которая не соответсвует дню недели");
+}
+else 
+{
+    if (number < 6) Console.WriteLine("Не является выходным днем");
+    if (number > 5) Console.WriteLine("Является выходным днем");
+}
